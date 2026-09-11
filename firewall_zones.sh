342f77c5b1fc522bcf4d59a5b6f93207a9f31bb1
APARNAMALATHI <174698753+APARNAMALATHI@users.noreply.github.com>
2026-09-11 11:11:30 +0530

    Update firewall_zones.sh

--- firewall_zones.sh
+++ firewall_zones.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Student Name:
-# Register Number:
+# Student Name:aparna
+# Register Number:1U24IT010
 
 # 1. Display all available firewall zones
 # TODO
-
+firewall-cmd --get-zones
 # 2. Display current default zone
 # TODO
+firewall-cmd --get-default-zone
 
 
 # 3. Display information about public zone
 # TODO
+firewall-cmd --zone=public --list-all
 
 # 4. Set default zone to internal
 # TODO
+firewall-cmd --set-default-zone=internal
 
 
 # 5. Verify the new default zone
 # TODO
-
+firewall-cmd --get-default-zone
 
 
 
